881607b19540d4bd42b6b38e1650125dc4c48e61
Koen Koedam <koedamkoen@gmail.com>
2023-01-31 21:03:08 +0100

    stuff

--- InitiativeTracker/initativeForms/initativeForms/Form1.Designer.cs
+++ InitiativeTracker/initativeForms/initativeForms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace initativeForms
 {
-    partial class Form1
+    partial class initiativeTrackerForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,169 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.initiativeLabel = new System.Windows.Forms.Label();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.typeCombobox = new System.Windows.Forms.ComboBox();
+            this.initiativeNumeric = new System.Windows.Forms.NumericUpDown();
+            this.initialsLabel = new System.Windows.Forms.Label();
+            this.enemyLabel = new System.Windows.Forms.Label();
+            this.enemyNumeric = new System.Windows.Forms.NumericUpDown();
+            this.initialTextbox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.killButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.connectionLabel = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.initiativeNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyNumeric)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // initiativeLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 95);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Initiative";
+            this.initiativeLabel.AutoSize = true;
+            this.initiativeLabel.Location = new System.Drawing.Point(12, 132);
+            this.initiativeLabel.Name = "initiativeLabel";
+            this.initiativeLabel.Size = new System.Drawing.Size(52, 15);
+            this.initiativeLabel.TabIndex = 0;
+            this.initiativeLabel.Text = "Initiative";
             // 
-            // label2
+            // typeLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Type";
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(12, 46);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(31, 15);
+            this.typeLabel.TabIndex = 1;
+            this.typeLabel.Text = "Type";
             // 
-            // comboBox1
+            // typeCombobox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.typeCombobox.FormattingEnabled = true;
+            this.typeCombobox.Items.AddRange(new object[] {
             "PC",
             "Enemy"});
-            this.comboBox1.Location = new System.Drawing.Point(105, 6);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(76, 23);
-            this.comboBox1.TabIndex = 2;
+            this.typeCombobox.Location = new System.Drawing.Point(105, 43);
+            this.typeCombobox.Name = "typeCombobox";
+            this.typeCombobox.Size = new System.Drawing.Size(76, 23);
+            this.typeCombobox.TabIndex = 2;
+            this.typeCombobox.SelectedIndexChanged += new System.EventHandler(this.typeCombobox_SelectedIndexChanged);
             // 
-            // numericUpDown1
+            // initiativeNumeric
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(105, 93);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(76, 23);
-            this.numericUpDown1.TabIndex = 3;
+            this.initiativeNumeric.Enabled = false;
+            this.initiativeNumeric.Location = new System.Drawing.Point(105, 130);
+            this.initiativeNumeric.Name = "initiativeNumeric";
+            this.initiativeNumeric.Size = new System.Drawing.Size(76, 23);
+            this.initiativeNumeric.TabIndex = 3;
             // 
-            // label3
+            // initialsLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 38);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 15);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Player initials";
+            this.initialsLabel.AutoSize = true;
+            this.initialsLabel.Location = new System.Drawing.Point(12, 75);
+            this.initialsLabel.Name = "initialsLabel";
+            this.initialsLabel.Size = new System.Drawing.Size(76, 15);
+            this.initialsLabel.TabIndex = 4;
+            this.initialsLabel.Text = "Player initials";
             // 
-            // label4
+            // enemyLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 66);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 15);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Enemy number";
+            this.enemyLabel.AutoSize = true;
+            this.enemyLabel.Location = new System.Drawing.Point(12, 103);
+            this.enemyLabel.Name = "enemyLabel";
+            this.enemyLabel.Size = new System.Drawing.Size(88, 15);
+            this.enemyLabel.TabIndex = 5;
+            this.enemyLabel.Text = "Enemy number";
             // 
-            // numericUpDown2
+            // enemyNumeric
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(105, 64);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(76, 23);
-            this.numericUpDown2.TabIndex = 6;
+            this.enemyNumeric.Enabled = false;
+            this.enemyNumeric.Location = new System.Drawing.Point(105, 101);
+            this.enemyNumeric.Name = "enemyNumeric";
+            this.enemyNumeric.Size = new System.Drawing.Size(76, 23);
+            this.enemyNumeric.TabIndex = 6;
             // 
-            // textBox1
+            // initialTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 35);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(76, 23);
-            this.textBox1.TabIndex = 7;
+            this.initialTextbox.Enabled = false;
+            this.initialTextbox.Location = new System.Drawing.Point(105, 72);
+            this.initialTextbox.Name = "initialTextbox";
+            this.initialTextbox.Size = new System.Drawing.Size(76, 23);
+            this.initialTextbox.TabIndex = 7;
             // 
-            // button1
+            // addButton
             // 
-            this.button1.Location = new System.Drawing.Point(12, 122);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Add to list";
-            this.button1.UseVisualStyleBackColor = true;
+            this.addButton.Location = new System.Drawing.Point(12, 159);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(169, 23);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Add to list";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
-            // listView1
+            // killButton
             // 
-            this.listView1.Location = new System.Drawing.Point(206, 6);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(177, 329);
-            this.listView1.TabIndex = 9;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.killButton.Enabled = false;
+            this.killButton.Location = new System.Drawing.Point(13, 274);
+            this.killButton.Name = "killButton";
+            this.killButton.Size = new System.Drawing.Size(168, 23);
+            this.killButton.TabIndex = 10;
+            this.killButton.Text = "Kill";
+            this.killButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // nextButton
             // 
-            this.button2.Location = new System.Drawing.Point(12, 209);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Kill";
-            this.button2.UseVisualStyleBackColor = true;
+            this.nextButton.Enabled = false;
+            this.nextButton.Location = new System.Drawing.Point(14, 245);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(167, 23);
+            this.nextButton.TabIndex = 11;
+            this.nextButton.Text = "Next turn";
+            this.nextButton.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // startButton
             // 
-            this.button3.Location = new System.Drawing.Point(13, 180);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(167, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Next turn";
-            this.button3.UseVisualStyleBackColor = true;
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(13, 216);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(168, 23);
+            this.startButton.TabIndex = 12;
+            this.startButton.Text = "Start combat";
+            this.startButton.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // resetButton
             // 
-            this.button4.Location = new System.Drawing.Point(12, 151);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(168, 23);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "Start combat";
-            this.button4.UseVisualStyleBackColor = true;
+            this.resetButton.Enabled = false;
+            this.resetButton.Location = new System.Drawing.Point(13, 326);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(168, 23);
+            this.resetButton.TabIndex = 13;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // removeButton
             // 
-            this.button5.Location = new System.Drawing.Point(12, 312);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(168, 23);
-            this.button5.TabIndex = 13;
-            this.button5.Text = "Reset";
-            this.button5.UseVisualStyleBackColor = true;
+            this.removeButton.Enabled = false;
+            this.removeButton.Location = new System.Drawing.Point(187, 326);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(196, 23);
+            this.removeButton.TabIndex = 15;
+            this.removeButton.Text = "Remove from list";
+            this.removeButton.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(109, 5);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(274, 23);
+            this.connectButton.TabIndex = 16;
+            this.connectButton.Text = "Connect to Arduino";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // connectionLabel
+            // 
+            this.connectionLabel.AutoSize = true;
+            this.connectionLabel.Location = new System.Drawing.Point(14, 9);
+            this.connectionLabel.Name = "connectionLabel";
+            this.connectionLabel.Size = new System.Drawing.Size(89, 15);
+            this.connectionLabel.TabIndex = 17;
+            this.connectionLabel.Text = "Not connected!";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(187, 43);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(196, 274);
+            this.listBox.TabIndex = 18;
+            // 
+            // initiativeTrackerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 347);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(395, 354);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.connectionLabel);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.killButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.initialTextbox);
+            this.Controls.Add(this.enemyNumeric);
+            this.Controls.Add(this.enemyLabel);
+            this.Controls.Add(this.initialsLabel);
+            this.Controls.Add(this.initiativeNumeric);
+            this.Controls.Add(this.typeCombobox);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.initiativeLabel);
+            this.Name = "initiativeTrackerForm";
+            this.Text = "Initiative Tracker";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initiativeNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyNumeric)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,19 +242,22 @@
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private ComboBox comboBox1;
-        private NumericUpDown numericUpDown1;
-        private Label label3;
-        private Label label4;
-        private NumericUpDown numericUpDown2;
-        private TextBox textBox1;
-        private Button button1;
-        private ListView listView1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
+        private Label initiativeLabel;
+        private Label typeLabel;
+        private ComboBox typeCombobox;
+        private NumericUpDown initiativeNumeric;
+        private Label initialsLabel;
+        private Label enemyLabel;
+        private NumericUpDown enemyNumeric;
+        private TextBox initialTextbox;
+        private Button addButton;
+        private Button killButton;
+        private Button nextButton;
+        private Button startButton;
+        private Button resetButton;
+        private Button removeButton;
+        private Button connectButton;
+        private Label connectionLabel;
+        private ListBox listBox;
     }
 }
--- InitiativeTracker/initativeForms/initativeForms/Form1.cs
+++ InitiativeTracker/initativeForms/initativeForms/Form1.cs
@@ -1,8 +1,8 @@
 namespace initativeForms
 {
-    public partial class Form1 : Form
+    public partial class initiativeTrackerForm : Form
     {
-        public Form1()
+        public initiativeTrackerForm()
         {
             InitializeComponent();
         }
@@ -10,6 +10,39 @@ namespace initativeForms
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+            if (typeCombobox.SelectedIndex == 0)
+            {
+                KeyValuePair<string, decimal> newCharacter = new KeyValuePair<string, decimal>(initialTextbox.Text, initiativeNumeric.Value);
+                listBox.Items.Add(string.Format("{0}: ({1})", newCharacter.Value, newCharacter.Key));
+            }
+            else if(typeCombobox.SelectedIndex == 1)
+            {
+                KeyValuePair<decimal, decimal> newEnemy = new KeyValuePair<decimal, decimal>(enemyNumeric.Value, initiativeNumeric.Value);
+                listBox.Items.Add(string.Format("{0}: (Enemy #{1})", newEnemy.Value, newEnemy.Key));
+            }
+            listBox.Sorted = true;
+        }
+
+        private void typeCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (typeCombobox.SelectedIndex) 
+            {
+                case 0: initialTextbox.Enabled = true;
+                    enemyNumeric.Enabled = false;
+                    initiativeNumeric.Enabled = true;
+                    break;
+                case 1: enemyNumeric.Enabled = true;
+                    initialTextbox.Enabled = false;
+                    initiativeNumeric.Enabled = true;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
